d9a1b0230f9bee6255a0f0ea441498db47c4860a
Shiva Shankar Malga(UST <285144@ust.com>
2025-07-02 13:11:41 +0530

    Incremental load logic added

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import os
 from dotenv import load_dotenv 
 import pandas as pd 
 from io import BytesIO
+from datetime import datetime
+cnt=0
+def is_processed_file(filename):
+    s=filename.split("_")
+    date_str=s[1]
+    previous=datetime.strptime(date_str,"%Y%m%d")
+    current=datetime.now()
+    diff=(current-previous).days
+    if(diff==1):
+        return True 
+    else:
+        return False 
 
 load_dotenv()
 try:
@@ -20,12 +32,19 @@ bucket_name = os.getenv("S3_BUCKET")
 # List all objects in the bucket
 response = s3.list_objects_v2(Bucket=bucket_name)
 
-# Loop through each file and process
-master_df=pd.DataFrame(columns=['timestamp','city','country','latitude','longitude','temperature','feels_like','humidity','pressure','weather_main','weather_description','wind_speed','wind_direction',
-'visibility','cloudiness'])
+master_file='weatherdata.csv'
+if os.path.exists(master_file):
+    master_df=pd.read_csv(master_file)
+else:
+    master_df=pd.DataFrame()
+
 for obj in response.get('Contents', []):
     key = obj['Key']
+    if(not is_processed_file(key)):
+        continue
+
     print(f"Processing file: {key}")
+    
     
     # Read file content directly
     file_obj = s3.get_object(Bucket=bucket_name, Key=key)
@@ -35,12 +54,19 @@ for obj in response.get('Contents', []):
     filtered_df=df.drop(['sunrise','sunset'],axis=1)
     filtered_df = filtered_df.astype({'wind_direction': 'float64', 'visibility': 'float64', 'cloudiness': 'float64'})
     filtered_df['timestamp'] = pd.to_datetime(filtered_df['timestamp'], errors='coerce')
-    master_df=pd.concat([master_df,filtered_df],ignore_index=True) 
+    filtered_df.to_csv(master_file, mode='a', header=not os.path.exists(master_file), index=False)
+    cnt+=1
+
+print("data is loaded into the master file")
+print(f"{cnt} files are processed!!")
 
 
-master_df.to_csv('weatherdatafile.csv',index=False)
-print("File is saved !!")
-    
+
+
+
+
+
+
 
 
 
